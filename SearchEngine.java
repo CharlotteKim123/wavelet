b837a498d00cf0177f9605b8c0719d409e169c35
CharlotteKim123 <charlottehkim@gmail.com>
2022-10-27 12:15:29 -0700

    changing

--- SearchEngine.java
+++ SearchEngine.java
@@ -2,19 +2,23 @@ import java.io.IOException;
 import java.net.URI;
 
 class Handler implements URLHandler {
-    // The one bit of state on the server: a number that will be manipulated by
-    // various requests.
     String str = "";
 
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
-            return String.format("String: %d", str);
+            return String.format("String: %s", str);
         } else {
             System.out.println("Path: " + url.getPath());
             if (url.getPath().contains("/add")) {
                 String[] parameters = url.getQuery().split("=");
                 str += parameters[1];
-                return String.format("The string is now %d", parameters[1], str);
+                return String.format("The string is now %s", parameters[1], str); 
+            }
+            if (url.getPath().contains("/search")){
+                String[] parameters = url.getQuery().split("=");
+                if (str.contains(parameters[1])){
+                    return String.format("The string is now %s", str, str);
+                }
             }
             return "404 Not Found!";
         }
